9881a88f302dacb9d048d049b2e9b48ede195649
Afiq Afifuddin <afiqafifuddin002@gmail.com>
2024-06-11 14:53:41 +0800

    Update Constants.java

--- app/src/main/java/com/example/foodorderingworkplace/Constants.java
+++ app/src/main/java/com/example/foodorderingworkplace/Constants.java
@@ -2,7 +2,7 @@ package com.example.foodorderingworkplace;
 
 public class Constants {
 
-    public static final String FCM_KEY = "AAAAo--l1eo:APA91bGMoTML3-nIa5g_PZxVm78jgcaPh-9a57-61SAwRbEz_g7OrjXt6btWqEL_L3ExEmTzDYbxmrPEl15cCuSTJybwZWXjJvlZZfO0WPypfO3wI2Ktod-Lvh3y_3rACmzbJ89lIAUe";
+    public static final String FCM_KEY = "";
 
     public static final String FCM_TOPIC = "PUSH_NOTIFICATIONS";
     //food categories
